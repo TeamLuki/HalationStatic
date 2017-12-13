24dd1d8e8d61bc84e9e624d984b0161ff614dced
Shana6 <atmatm6000@outlook.com>
2017-12-12 21:34:15 -0600

    here

---
+++ register/validate.js
@@ -0,0 +1,7 @@
+function validate(){
+    var w = document.forms["register"]["usern"].value;
+    var x = document.forms["register"]["email"].value;
+    var y = document.forms["register"]["passw"].value;
+    var z = document.forms["register"]["confi"].value;
+    if (w.length == 0)
+}
